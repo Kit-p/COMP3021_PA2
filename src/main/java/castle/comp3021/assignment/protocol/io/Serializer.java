36fea40b55d6fd8114c71d1affe0e12eadc43222
Kit-p <kpangaa@connect.ust.hk>
2020-11-05 11:37:03 +0800

    implement Serializer.saveToFile()

--- src/main/java/castle/comp3021/assignment/protocol/io/Serializer.java
+++ src/main/java/castle/comp3021/assignment/protocol/io/Serializer.java
@@ -2,9 +2,14 @@ package castle.comp3021.assignment.protocol.io;
 
 
 import castle.comp3021.assignment.gui.FXJesonMor;
+import javafx.scene.control.Alert;
+import javafx.stage.FileChooser;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * This class exports the entire game configuration and procedure to file
@@ -33,6 +38,41 @@ public class Serializer {
      */
     public void saveToFile(FXJesonMor fxJesonMor) throws IOException {
         //TODO
+        FileChooser fileChooser = new FileChooser();
+        var mapFilter = new FileChooser.ExtensionFilter("Move Record File", "*.map");
+        fileChooser.setSelectedExtensionFilter(mapFilter);
+        File file = fileChooser.showSaveDialog(null);
+        if (file != null) {
+            writeToFile(file, fxJesonMor.toString());
+        }
+    }
+
+    /**
+     * Helper method for writing content to a file.
+     * @param file the file to be written to
+     * @param content the content to be written to
+     */
+    private static void writeToFile(File file, String content) throws IOException {
+        boolean isSuccess;
+        if (file.exists()) {
+            isSuccess = file.delete();
+            if (!isSuccess) {
+                throw new IOException("Cannot delete existing file!");
+            }
+        }
+        isSuccess = file.createNewFile();
+        if (!isSuccess) {
+            throw new IOException("Cannot create new file!");
+        }
+        try {
+            PrintWriter writer = new PrintWriter(file);
+            writer.print(content);
+        } catch (FileNotFoundException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Cannot save move record to file!");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
+        }
     }
 
 }
